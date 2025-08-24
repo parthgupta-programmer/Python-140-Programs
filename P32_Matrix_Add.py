a98d51157f0b8c08d6b1a11a7da93d3acdfd99e9
parthgupta-programmer <parthg.info@gmail.com>
2025-08-24 19:07:36 +0530

    Addition of Two matrices

---
+++ P32_Matrix_Add.py
@@ -0,0 +1,30 @@
+# Program 37
+# Write a Python Program to Add Two Matrix.
+
+rows=int(input("Enter number of rows:"))
+colmns=int(input("Enter number of columns:"))
+
+print("Enter data row by row:")
+
+matrix1=[]
+matrix2=[]
+
+print("Enter Matrix 1 Elements:")
+for i in range(rows):
+    l=list(map(int,input().split()))
+    matrix1.append(l)
+
+print("Enter Matrix 2 Elements:")
+for i in range(rows):
+    l=list(map(int,input().split()))
+    matrix2.append(l)
+
+r=[]
+for i in range(rows):
+    l=[]
+    for j in range(colmns):
+        l.append(matrix1[i][j]+matrix2[i][j])
+        print(matrix1[i][j]+matrix2[i][j],end=" ")
+    print()
+    r.append(l)
+print(r)
